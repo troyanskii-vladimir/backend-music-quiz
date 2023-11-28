626eab8488850bcf83211f5584e2870a58220b30
Vladimir <troyanskii1998@yandex.ru>
2023-11-29 01:21:29 +0300

    Доабвляет маршруты для создания и редактирования пользователей

---
+++ server/config/mongo.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+/////////ДОБАВИТЬ ЮРЛ НА БД
+
+const CONNECTION_URL = `mongodb+srv://troyanskii1998:xtsOdQS8eRfhSlgz@cluster0.tvrvrc0.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongo has connected succesfully')
+})
+mongoose.connection.on('reconnected', () => {
+  console.log('Mongo has reconnected')
+})
+mongoose.connection.on('error', error => {
+  console.log('Mongo connection has an error', error)
+  mongoose.disconnect()
+})
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongo connection is disconnected')
+})
