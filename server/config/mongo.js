275e282b4ccf664ef8a302cab5640f6a594a0341
Vladimir <troyanskii1998@yandex.ru>
2023-12-07 22:30:56 +0300

    рефакторинг

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -1,22 +1,22 @@
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
-const CONNECTION_URL='mongodb+srv://troyanskii1998:xtsOdQS8eRfhSlgz@cluster0.tvrvrc0.mongodb.net/?retryWrites=true&w=majority';
+// const CONNECTION_URL='mongodb+srv://troyanskii1998:xtsOdQS8eRfhSlgz@cluster0.tvrvrc0.mongodb.net/?retryWrites=true&w=majority';
 
-mongoose.connect(CONNECTION_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+// mongoose.connect(CONNECTION_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
 
-mongoose.connection.on('connected', () => {
-  console.log('Mongo has connected succesfully')
-})
-mongoose.connection.on('reconnected', () => {
-  console.log('Mongo has reconnected')
-})
-mongoose.connection.on('error', error => {
-  console.log('Mongo connection has an error', error)
-  mongoose.disconnect()
-})
-mongoose.connection.on('disconnected', () => {
-  console.log('Mongo connection is disconnected')
-})
+// mongoose.connection.on('connected', () => {
+//   console.log('Mongo has connected succesfully')
+// })
+// mongoose.connection.on('reconnected', () => {
+//   console.log('Mongo has reconnected')
+// })
+// mongoose.connection.on('error', error => {
+//   console.log('Mongo connection has an error', error)
+//   mongoose.disconnect()
+// })
+// mongoose.connection.on('disconnected', () => {
+//   console.log('Mongo connection is disconnected')
+// })
--- server/index.js
+++ server/index.js
@@ -52,109 +52,126 @@ let rooms = [];
 
 let privateRooms = [];
 
+import { socker } from './utils/index.js';
+
 /** Create HTTP server. */
 const server = createServer(app);
 
 /** Create socket connection */
-const io = new Server(server, {
-  cors: {
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'DELETE', 'PUT'],
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: 'http://localhost:5173',
+//     methods: ['GET', 'POST', 'DELETE', 'PUT'],
+//   },
+// });
 
-const makeQuestionsForGame = async (packId) => {
-  const questions = await QuestionModel.getQuestionsByPackId(packId);
-  const songs = await questions.map((question) => question.song);
-  const artists = await questions.map((question) => question.artist);
+// const makeQuestionsForGame = async (packId) => {
+//   const questions = await QuestionModel.getQuestionsByPackId(packId);
+//   const songs = await questions.map((question) => question.song);
+//   const artists = await questions.map((question) => question.artist);
 
-  return Array.from({ length: 10 }, () => {
-    return {
-      question: questions[Math.floor(Math.random() * questions.length)],
-      wrongSongs: Array.from({ length: 3 }, () => songs[Math.floor(Math.random() * songs.length)]),
-      wrongArtists: Array.from({ length: 3 }, () => artists[Math.floor(Math.random() * artists.length)]),
-    }
-  });
-};
+//   return Array.from({ length: 10 }, () => {
+//     return {
+//       question: questions[Math.floor(Math.random() * questions.length)],
+//       wrongSongs: Array.from({ length: 3 }, () => songs[Math.floor(Math.random() * songs.length)]),
+//       wrongArtists: Array.from({ length: 3 }, () => artists[Math.floor(Math.random() * artists.length)]),
+//     }
+//   });
+// };
+
+// const roundGameProcess = async (roomId, roundQuestion, roundNumber) => {
+//   const dataToSend = {
+//     songUrl: roundQuestion.question.audioFileUrl,
+//     artists: roundQuestion.wrongArtists.concat([roundQuestion.question.artist]),
+//     songs: roundQuestion.wrongSongs.concat([roundQuestion.question.song]),
+//     roundNumber,
+//   };
+
+//   io.to(roomId).emit('recieve_gameData', dataToSend);
+// };
+
+// const mainGameProccess = async (roomId, packId) => {
+//   const roundQuestions = await makeQuestionsForGame(packId);
+
+//   let count = 0;
+
+//   const test = setInterval(() => {
+//     roundGameProcess(roomId, roundQuestions[count], count);
+//     count++;
+//   }, 5000)
+// };
+
+// class User {
+//   constructor(question) {
+//     question;
+//     this.question = question;
+//   }
+
+//   onClose = () => {
+//     console.log('test');
+//   }
+// }
+
+// const user = new User()
 
 
-io.on('connection', (client) => {
-  const roundGameProcess = async (roomId, roundQuestion, roundNumber) => {
-    const dataToSend = {
-      songUrl: roundQuestion.question.audioFileUrl,
-      artists: roundQuestion.wrongArtists.concat([roundQuestion.question.artist]),
-      songs: roundQuestion.wrongSongs.concat([roundQuestion.question.song]),
-      roundNumber,
-    };
+// io.on('connection', (client) => {
 
-    io.to(roomId).emit('recieve_gameData', dataToSend);
+//   client.on('send_my_answer', (data) => {
+//     console.log('Ответ получен', client.id, data);
+//   })
 
-    client.on('send_my_answer', (data) => {
-      console.log('Ответ получен', data, roundNumber)
-    })
-  };
 
-  const mainGameProccess = async (roomId, packId) => {
-    const roundQuestions = await makeQuestionsForGame(packId);
+//   client.on("disconnect", () => {
+//     users = users.filter((user) => user.socketId !== client.id);
+//   });
 
-    let count = 0;
+//   client.on("identity", (userId) => {
+//     console.log(users)
+//     if (userId === null) {
+//       return;
+//     }
+//     users = users.filter((user) => user.userId !== userId);
+//     users.push({
+//       socketId: client.id,
+//       userId: userId,
+//     })
+//   });
 
-    const test = setInterval(() => {
-      roundGameProcess(roomId, roundQuestions[count], count);
-      count++;
-    }, 5000)
-  };
+//   client.on('get_onlineUsers', () => {
+//     client.emit('recieve_onlineUsers', users)
+//   })
 
-  client.on("disconnect", () => {
-    users = users.filter((user) => user.socketId !== client.id);
-  });
+//   client.on('create_room', (data) => {
+//     const { _id, packImageUrl, packName } = data;
 
-  client.on("identity", (userId) => {
-    console.log(users)
-    if (userId === null) {
-      return;
-    }
-    users = users.filter((user) => user.userId !== userId);
-    users.push({
-      socketId: client.id,
-      userId: userId,
-    })
-  });
+//     rooms.push({
+//       roomId: _id,
+//       roomName: packName,
+//     })
 
-  client.on('get_onlineUsers', () => {
-    client.emit('recieve_onlineUsers', users)
-  })
+//     mainGameProccess(_id, _id);
+//   });
 
-  client.on('create_room', (data) => {
-    const { _id, packImageUrl, packName } = data;
+//   client.on('join_room', (roomId) => {
+//     // const roomIds = rooms.map((room) => room.roomId);
+//     // if (!roomIds.includes(roomId)) {
+//     //   return;
+//     // }
+//     console.log(`Засунули тебя в команту ${roomId}`)
 
-    rooms.push({
-      roomId: _id,
-      roomName: packName,
-    })
+//     client.join(roomId);
+//   })
 
-    mainGameProccess(_id, _id);
-  });
+//   client.on("subscribe", (room, otherUserId = "") => {
+//     this.subscribeOtherUser(room, otherUserId);
+//     client.join(room);
+//   });
 
-  client.on('join_room', (roomId) => {
-    // const roomIds = rooms.map((room) => room.roomId);
-    // if (!roomIds.includes(roomId)) {
-    //   return;
-    // }
-    console.log(`Засунули тебя в команту ${roomId}`)
-
-    client.join(roomId);
-  })
-
-  client.on("subscribe", (room, otherUserId = "") => {
-    this.subscribeOtherUser(room, otherUserId);
-    client.join(room);
-  });
-
-  client.on("unsubscribe", (room) => {
-    client.leave(room);
-  });
-});
+//   client.on("unsubscribe", (room) => {
+//     client.leave(room);
+//   });
+// });
 
 /** Listen on provided port, on all network interfaces. */
 server.listen(port);
